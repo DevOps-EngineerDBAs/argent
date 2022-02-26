65109646ff014e2a19d1b6f606c4a6c86aca4716
oliviaf <lividevops@gmail.com>
2022-02-25 23:32:15 -0500

    This is my first commit

---
+++ bitcoin.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+#This is a project call argent in collaboration with LIV MIMI,JULIE and FRAN
+echo "what is your name"
+read name
+echo "what is your currency"
+read name
+echo "Thank you for providing your currency name" 
+
