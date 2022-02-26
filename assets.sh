4f7c21b0995eeba15be652ae7bb673b623521650
francis <fmbita@yahoo.fr>
2022-02-26 04:30:03 +0000

    this is the original script

---
+++ assets.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Please enter your favorite car1"
+read car1
+echo "Please enter your favorite car2"
+read car2
+echo "Please enter your last favorite car3"
+read car3
+echo "this is the list of your favorite cars"
+echo "1- $car1"
+echo "2- $car2"
+echo "3- $car3"
